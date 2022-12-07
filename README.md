3a913d45f9a0ed7ab73e9e429b162eb064ec5329
enricoargenti <enrico.argenti@stud.itsaltoadriatico.it>
2022-12-07 17:52:26 +0100

    AMQP communication started

--- README.md
+++ README.md
@@ -4,7 +4,11 @@
 ## **Stato dell'arte:**  
 
 
-
+### AMQP WORKING...
+Comunicazione dati su Redis ed estrazione dallo stesso: COMPLETATA
+Comunicazione dati da producer a RabbitMQ : implementata connessione con CloudAmqp, ma c'è 
+una mancanza sul binding che non permette di mandare i dati con successo
+Comunicazione dati da RabbitMQ ai consumers: da fare. 
 
 ### Generazione casuale ed invio misurazioni drone:
 
@@ -23,59 +27,3 @@ Il *payload* corrisponde ad un pacchetto JSON contenente:
 * Value (valore rilevato)
 * Time (data e ora di rilevazione del dato)
 
-
- 
-### Broker MQTT:
-
-A garanzia della comunicazione tramite protocollo MQTT si sfrutta un broker Mosquitto 
-(che viene fatto girare in locale attraverso Docker). 
-
-
-    
-### Ricezione ed elaborazione/storage dati drone:
-    
-Dal lato della ricezione dei dati dei sensori abbiamo tre diverse modalità di gestione:
-
-* dataStorage: un client `subscriberDataStorage.js` riceve tutti i dati generati da tutti i droni
-attaverso l'iscrizione al topic `iot2022test/#` e li salva sul database *Postgres* 
-sfruttando le chiamate del *provider* `provider.js`;
-
-* positionController: un client dedicato `positionSubscriber.js` che sfruttando l'opzione *flag retain*
-restituisce l'ultima posizione disponibile di un determinato drone (la situazione immaginata è quella
-in cui il drone non dovesse più rispondere e si volesse capire in che punto qualcosa è andato storto);
-
-* speedController: un client dedicato `subscriberSpeed.js` per la gestione dei dati della velocità 
-di tutti i droni (nelle intenzioni ogni tipo di misurazione proveniente dal drone 
-verrà gestita con un subscriber apposito). 
-
-
-    
-### Invio di comandi al drone da remoto: 
-
-Per la generazione e l'invio di comandi verso il drone si è sviluppato 
-invece un commandsSender `commandsSender.js` che pubblica i comandi inseriti da shell 
-dall'utente sul *topic* `iot2022test/commands/drone_id`. 
-Il *payload* per ora corrisponde ad una semplice stringa contenente un comando. 
-Successivamente si dovrà aggiungere un id per il comando (per garantire che
-si possa gestire la risposta asincrona di avvenuta esecuzione dello stesso). 
-
-    
-### Ricezione dei comandi da parte del drone:
-
-Dal lato client del drone l'iscrizione viene fatta al topic `iot2022test/commands/#`
-(per ora come test si ricevono tutti i comandi inviati a qualsiasi drone). 
-
-Si è riscotrato un problema nella gestione dei messaggi in arrivo da questo topic:
-nei prossimi giorni si cercherà di implementare un metodo per mandare al cloud
-un messaggio di avvenuta ricezione del comando sul *topic* `iot2022test/confirmCommands/#`
-con payload del tipo "Ho eseguito il comando {*comando*} a seguito della richiesta {*id_comando*}". 
-Si dovrà in futuro implementare la gestione dei messaggi ricevuti 
-successivamente all'iscrizione al topic `iot2022test/commands/#`
-
-
-    
-### TODOS: 
-
-Da fare: 
-* l'interfaccia grafica di visualizzazione dello stato del drone
-* sostituzione del db Postgres con InfluxDb
--- client/NetCoreClient/DataSender/Program.cs
+++ client/NetCoreClient/DataSender/Program.cs
@@ -1,18 +1,82 @@
-﻿using StackExchange.Redis;
+﻿using RabbitMQ.Client;
+using System.Text;
+using DataSender;
+using StackExchange.Redis;
 
-
-
-// Connection to Redis
-ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
-IDatabase db = redis.GetDatabase();
-
-
-const string key = "packet"; //forse da sostituire con droneId perché nel pacchetto non c'è
-
-string? result = "pending";
-
-while(result != null)
+class Program
 {
-     result = await db.ListLeftPopAsync(key);
-    Console.WriteLine("Direttamente da Redis: " + result);
+    // The url of the AMQP server, should look like amqps://[username]:[password]@[instance]/[vhost]
+    static readonly string _url = "amqps://erzcddln:Noq08Uww2NHycbvZHuarnYOZbYdigSQG@whale.rmq.cloudamqp.com/erzcddln";
+    static ManualResetEvent _quitEvent = new ManualResetEvent(false);
+
+    public static async Task<int> Main(string[] args)
+    {
+        var url = _url;
+        if (args.Length > 0)
+            url = args[0];
+
+        Console.CancelKeyPress += (sender, eArgs) => {
+            // set the quit event so that the Consumer will receive it and quit gracefully
+            _quitEvent.Set();
+            Console.WriteLine("CancelEvent received, shutting down...");
+            // sleep 1 second to give Consumer time to clean up
+            Thread.Sleep(1000);
+        };
+
+        // setup worker thread
+        var consumer = new Consumer(url, _quitEvent);
+        var consumerThread = new Thread(consumer.ConsumeQueue) { IsBackground = true };
+        consumerThread.Start();
+
+        // create a connection and open a channel, dispose them when done
+        var factory = new ConnectionFactory
+        {
+            Uri = new Uri(url)
+        };
+
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+        // ensure that the queue exists before we publish to it
+        var queueName = "queue1";
+        bool durable = true;
+        bool exclusive = false;
+        bool autoDelete = false;
+
+        channel.QueueDeclare(queueName, durable, exclusive, autoDelete, null);
+
+        //--------------------------------------------------------------------------------------------------
+                                            //Prelevo da Radis
+        // Connection to Redis
+        ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+        IDatabase db = redis.GetDatabase();
+
+        const string key = "packet"; //forse da sostituire con droneId perché nel pacchetto non c'è
+
+        string? packet = "pending";
+        //--------------------------------------------------------------------------------------------------
+        while (true)
+        {
+            //Console.WriteLine("Enter a message and publish with pressing the return key (exit with ctrl-c)");
+            //var message = Console.ReadLine();
+            packet = await db.ListLeftPopAsync(key);
+
+            if (packet != null)
+            {
+                Console.WriteLine("Direttamente da Redis: " + packet);
+
+                // the data put on the queue must be a byte array
+                var data = Encoding.UTF8.GetBytes(packet);
+                // publish to the topic exchange
+                channel.ExchangeDeclare("myExchange", "topic"); //exchange set with type Topic
+                var routingKey = queueName;
+
+                channel.QueueBind("queue1", "myExchange", routingKey);
+
+                channel.BasicPublish("myExchange", routingKey, null, data);
+                Console.WriteLine("Published message {0}\n", packet);
+            }
+
+            
+        }
+    }
 }
