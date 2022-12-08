e9024eda43c9ee35f8f5e20bd02a25d7bfcffc94
enricoargenti <enrico.argenti@stud.itsaltoadriatico.it>
2022-12-08 12:39:02 +0100

    Connection to db completed

--- cloud/dataStorage/consumerDataStorage.js
+++ cloud/dataStorage/consumerDataStorage.js
@@ -13,11 +13,57 @@ const main_queue = "drones.measurements";
   await ch1.assertQueue(queue);
 
   // Listener
-  ch1.consume(queue, (msg) => {
-    if (msg !== null) {
-      console.log('Received:', msg.content.toString());
+  ch1.consume(queue, async (msg) => {
+    if (msg !== null) 
+    {
+      //console.log('Received:', msg.content.toString());
+
+      var message = JSON.parse(msg.content.toString());
+      //console.log("Parsed JSON: " + message);
+      var droneID = JSON.parse(message.Drone_ID);
+      var data = JSON.parse(message.Data);
+
+      // insertion into the db calling a provider
+      var newStatus = {};
+
+      newStatus.droneID = droneID; //gets the id at the second position on topic
+      newStatus.type  = data.Type;
+      newStatus.time = data.Time;
+
+      //saves JSON packets that contain useful information about the drone status
+      var newJSON = {};
+      if(newStatus.type == "Position")
+      {
+          newJSON.Latitude = data.Latitude;
+          newJSON.Longitude = data.Longitude;
+      }
+      else
+      {
+          newJSON.Value = data.Value;
+      }
+      newStatus.dataJSON = JSON.stringify(newJSON);
+
+
+      // prints to check values
+      console.log("DroneID: " + newStatus.droneID);
+      console.log("Type: " + newStatus.type);
+      console.log("Time: " + newStatus.time);
+      console.log("dataJSON: " + newStatus.dataJSON);
+      console.log();
+
+      // it adds the new status
+      var last = await provider.addStatus(newStatus);
+      if(last == undefined) {
+          console.log("ERRORE di trasmissione al database: internalServerError");
+          //throw internalServerError();
+      }
+      //res.code(201);
+
+
       ch1.ack(msg);
-    } else {
+    } 
+    else 
+    {
       console.log('Consumer cancelled by server');
     }
   });
